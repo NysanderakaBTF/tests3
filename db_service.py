d8c13ccbb76b508019bf96b5fb4fe872fab9a159
Ryhor <kave7771@outlook.com>
2023-09-21 22:19:42 +0300

    pass test create

--- db_service.py
+++ db_service.py
@@ -9,3 +9,8 @@ class DBService:
     async def get_all_questions(cls):
         return await Question.filter().all()
 
+    @classmethod
+    async def create_question(cls, question, answer):
+        q = Question(question=question, answer=answer)
+        await q.save()
+        return q
